6c5d1495acfeac12b5294502035589b7df303233
huongntt1 <huongntt1@etc.vn>
2025-12-17 16:50:30 +0700

    feat:don hang

---
+++ frontend/src/views/don-hang/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <!-- <div>Đây là trang đơn hàng</div> -->
+    <a-form :model="formDonHang" layout="vertical">
+        <a-row gutter="16">
+            <a-col span="6">
+                <a-form-item label="Họ tên">
+                    <a-input></a-input>
+                </a-form-item>
+            </a-col>
+            <a-col span="6">
+                <a-form-item label="SĐT">
+                    <a-input></a-input>
+                </a-form-item>
+            </a-col>
+            <a-col span="12">
+                <a-form-item label="Địa chỉ">
+                    <a-input></a-input>
+                </a-form-item>
+            </a-col>
+        </a-row>
+        <a-row>
+            <a-col span="24">
+                <a-form-item label="Ghi chú">
+                    <a-textarea></a-textarea>
+                </a-form-item>
+            </a-col>
+        </a-row>
+        <table-cake />
+    </a-form>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import TableCake from './components/table.vue';
+
+const formDonHang = ref();
+</script>
