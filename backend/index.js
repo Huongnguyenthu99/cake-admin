ac26b7819a111806fc90add21159dd420057b0e5
huongntt1 <huongntt1@etc.vn>
2026-01-09 16:35:52 +0700

    fix: health check BR

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).send("OK");
+});
+
+
 app.use("/api", routes);
 
 app.listen(PORT, () => {
